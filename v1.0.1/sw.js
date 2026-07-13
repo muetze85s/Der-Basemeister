e6ed0b2c9f632d0de2180854b80b58c6dd10705c
muetze85s <56439144+muetze85s@users.noreply.github.com>
2026-07-13 07:08:20 +0700

    Create sw.js

---
+++ v1.0.1/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'detox-portal-v1.0.1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
